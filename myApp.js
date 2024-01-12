9845201f588c8ceb4292d6dd9b777d5c1446eae5
aridane <aridanecabreraramirez@gmail.com>
2024-01-12 18:24:57 +0100

    Serve an HTML File 4

--- myApp.js
+++ myApp.js
@@ -4,7 +4,7 @@ let app = express();
 // app.get("/", function (req, res) {
 //   res.send("Hello Express");
 // });
-
+const path =__dirname + "/views/index.html";
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/views/index.html");
+  res.sendFile(path);
 });
