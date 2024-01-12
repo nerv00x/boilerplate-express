ba6eb2ef217a0cdc95f52c30b6c29137383be2e2
aridane <aridanecabreraramirez@gmail.com>
2024-01-12 18:45:09 +0100

    Serve JSON on a Specific Route

--- myApp.js
+++ myApp.js
@@ -4,15 +4,20 @@ let app = express();
 // app.get("/", function (req, res) {
 // res.send("Hello Express");
 // });
-// 
-app.get("/", function(req, res) {
-    res.sendFile(__dirname + "/views/index.html");
+//
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/index.html");
 });
-
 
 // Normal usage
 // app.use(express.static(__dirname + "/public"));
 // Assets at the /public route
 app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/json", (req, res) => {
+  res.json({
+    message: "Hello json",
+  });
+});
 
 module.exports = app;
