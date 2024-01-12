7e1c708bc333c9dfdd1542b6d19e6de2b28f6f6c
aridane <aridanecabreraramirez@gmail.com>
2024-01-12 18:40:43 +0100

    Serve Static Assets 2

--- myApp.js
+++ myApp.js
@@ -7,12 +7,12 @@ let app = express();
 // 
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/views/index.html");
-  });
+});
+
 
 // Normal usage
-app.use(express.static(__dirname + "/public"));
-
+// app.use(express.static(__dirname + "/public"));
 // Assets at the /public route
-app.use("/public", express.static(__dirname + "/public/style.css"));
+app.use("/public", express.static(__dirname + "/public"));
 
 module.exports = app;
